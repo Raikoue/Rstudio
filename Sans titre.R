0d372f52d68e9b96ffdfd10ecbf5507e7ed2495f
Raikoue <143769768+Raikoue@users.noreply.github.com>
2024-02-12 16:29:57 -0500

    Add files via upload

---
+++ Sans titre.R
@@ -0,0 +1,8 @@
+#test
+## install if needed (do this exactly once):
+## install.packages("usethis")
+
+library(usethis)
+use_git_config(user.name = "Raikoue", user.email = "maxime.deplechin@usherbrooke.ca")
+
+gitcreds::gitcreds_set()
